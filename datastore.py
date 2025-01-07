7466cd6351e5920df7887ae1775160ad79b0702c
Vgnls <2849185831@qq.com>
2025-01-07 10:30:20 +0800

    datastore_update

--- datastore.py
+++ datastore.py
@@ -4,6 +4,102 @@ from tools import task
 from prettytable import PrettyTable
 
 
+def delete(si, datastore_name, datacenter_name=None):
+    """
+    Delete a datastore in a specific datacenter.
+
+    :param si: Service instance object connected to vCenter
+    :param datastore_name: Name of the datastore to be deleted
+    :param datacenter_name: Name of the datacenter where the datastore is located
+    :return: none
+    """
+    content = si.RetrieveContent()
+
+    container_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.Datacenter], True)
+    datacenters = list(container_view.view)
+
+    datacenter = None
+    # locate the datacenter
+    for datacenter_temp in datacenters:
+        if datacenter_temp.name == datacenter_name:
+            datacenter = datacenter_temp
+            break
+    container_view.Destroy()
+
+    if not datacenter:
+        raise ManagedObjectNotFoundError(
+            f"Managed object of type '[vim.Datacenter]' with name '{datacenter_name}' not found."
+        )
+
+    datastore = None
+    # locate the datastore by name
+    datastores = datacenter.datastore
+    for datastore_temp in datastores:
+        if datastore_temp.name == datastore_name:
+            datastore = datastore_temp
+            break
+
+    if not datastore:
+        raise ManagedObjectNotFoundError(
+            f"Managed object of type '[vim.Datastore]' with name '{datastore_name}' not found in datacenter"
+            f" '{datacenter_name}'."
+        )
+
+    # delete the datastore
+    tasks = [datastore.Destroy_Task()]
+    task.wait_for_tasks(si, tasks)
+    print(f"Datastore '{datastore_name}' deleted successfully.")
+
+
+def rename(si, datastore_name, new_name, datacenter_name):
+    """
+    Rename a datastore in a specific datacenter.
+
+    :param si: Service instance object connected to vCenter
+    :param datastore_name: Name of the datastore to be renamed
+    :param new_name: New name for the datastore
+    :param datacenter_name: Name of the datacenter where the datastore is located
+    :return: none
+    """
+    content = si.RetrieveContent()
+
+    # locate the datacenter by name
+    container_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.Datacenter], True)
+    datacenters = list(container_view.view)
+
+    datacenter = None
+    # locate the datacenter
+    for datacenter_temp in datacenters:
+        if datacenter_temp.name == datacenter_name:
+            datacenter = datacenter_temp
+            break
+    container_view.Destroy()
+
+    if not datacenter:
+        raise ManagedObjectNotFoundError(
+            f"Managed object of type '[vim.Datacenter]' with name '{datacenter_name}' not found."
+        )
+
+    datastore = None
+    # locate the datastore by name
+    datastores = datacenter.datastore
+    for datastore_temp in datastores:
+        if datastore_temp.name == datastore_name:
+            datastore = datastore_temp
+            break
+
+    if not datastore:
+        raise ManagedObjectNotFoundError(
+            f"Managed object of type '[vim.Datastore]' with name '{datastore_name}' not found in datacenter"
+            f" '{datacenter_name}'."
+        )
+
+    # rename the datastore
+    tasks = [datastore.Rename_Task(new_name)]
+    task.wait_for_tasks(si, tasks)
+    print(f"Datastore '{datastore_name}' renamed to '{new_name}' successfully.")
+
+
 def info(si, datastore_name, datacenter_name):
     """
     Display information about a specific datastore in a datacenter.
